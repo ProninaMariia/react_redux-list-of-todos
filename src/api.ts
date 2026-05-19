489a1ba9206579dbf94f3407397676b28a5d825c
Mariia <proninamaria2002@gmail.com>
2026-05-19 13:52:04 +0300

    Pronina-lasone

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,25 @@
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
+// eslint-disable-next-line operator-linebreak
 const BASE_URL =
-  'https://mate.academy/students-projects/react_dynamic-list-of-todos/api';
+  'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
-function get<T>(url: string): Promise<T> {
-  return fetch(`${BASE_URL}${url}`).then((res: Response) => res.json());
+function wait(delay: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
+  });
 }
 
-export const getTodos = (): Promise<Todo[]> => get<Todo[]>('/todos');
+function get<T>(url: string): Promise<T> {
+  // eslint-disable-next-line prefer-template
+  const fullURL = BASE_URL + url + '.json';
 
-export const getUser = (userId: number): Promise<User> =>
-  new Promise(resolve => setTimeout(resolve, 0)).then(() =>
-    get<User>(`/users/${userId}`),
-  );
+  return wait(300)
+    .then(() => fetch(fullURL))
+    .then(res => res.json());
+}
+
+export const getTodos = () => get<Todo[]>('/todos');
+
+export const getUser = (userId: number) => get<User>(`/users/${userId}`);
